449692c0a4551f7454e23c416609478e28bc65f0
Justin Vos <justinvosnz@gmail.com>
2016-04-21 22:27:25 +1200

    First code commit, including base UI code.

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title>routine</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet' href='style.css'>
+  </head>
+
+  <body>
+    <div id='head_outer'>
+      <div id='head_left'>
+        <input type='text' placeholder='search'>
+      </div>
+      <div id='head_main'>
+        <ul>
+          <li><input type='text' placeholder='label'></li>
+          <a><li>New</li></a>
+          <a><li>Reset</li></a>
+        </ul>
+      </div>
+    </div>
+
+    <div id='body_outer'>
+      <div id='side_outer'>
+        <ul>
+          <li>CS 230 Assignment 1</li>
+          <li>Daily schedule</li>
+          <li>CS 230 Assignment 2</li>
+        </ul>
+      </div>
+
+      <div id='main_outer'>
+        <ul>
+          <li>Document Amount class.</li>
+          <li class='checked'>Document Currency class.</li>
+          <li>Document Currencies class.</li>
+          <li>Document CurrencyExchangeMenu class.</li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
